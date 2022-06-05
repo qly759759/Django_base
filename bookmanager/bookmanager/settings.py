5ca21d1e9c9bc056b2d31039648d7ac5e633d799
qly759759 <502611360@qq.com>
2022-06-05 16:29:34 +0800

    how to creat son-app and register

--- bookmanager/bookmanager/settings.py
+++ bookmanager/bookmanager/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+import bookmanager.book.apps
+
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
@@ -37,6 +39,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'book.apps.BookConfig',
+
+
 ]
 
 MIDDLEWARE = [
