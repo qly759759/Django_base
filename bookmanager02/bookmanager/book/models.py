915598d1d7e04b543ec5c60492777fb57de63e7a
qly759759 <502611360@qq.com>
2022-06-12 21:56:03 +0800

    模板 类生成表

--- bookmanager02/bookmanager/book/models.py
+++ bookmanager02/bookmanager/book/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+
+class BookInfo(models.Model):
+    name = models.CharField(max_length=10,unique=True)
+    pub_data = models.DateField(null = True)
+    readcount = models.IntegerField(default=0)
+    commentcount =models.IntegerField(default=0)
+    is_delete = models.BooleanField(default=False)
+
+    class Meta:
+        db_table = 'bookinfo'
+        verbose_name = '书籍管理'
