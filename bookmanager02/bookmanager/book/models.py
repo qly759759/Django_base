ad536fd6a0c9a99029e2c2dfef2fb773ff86ad89
qly759759 <502611360@qq.com>
2022-06-17 20:09:41 +0800

    sql

--- bookmanager02/bookmanager/book/models.py
+++ bookmanager02/bookmanager/book/models.py
@@ -10,6 +10,29 @@ class BookInfo(models.Model):
     commentcount =models.IntegerField(default=0)
     is_delete = models.BooleanField(default=False)
 
+    def __str__(self):
+        return self.name
+
     class Meta:
         db_table = 'bookinfo'
         verbose_name = '书籍管理'
+
+class PeopleInfo(models.Model):
+    GENDER_CHOICE = {
+        ('1','male'),
+        ('2','female')
+    }
+
+    name = models.CharField(max_length=10, unique=True)
+    gender = models.SmallIntegerField(choices=GENDER_CHOICE, default=1)
+    description = models.CharField(max_length=100, null=True)
+    is_delete = models.BooleanField(default=False)
+
+    book = models.ForeignKey(BookInfo, on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = 'peopleinfo'
+        verbose_name = '人员管理'
+
+    def __str__(self):
+        return self.name
